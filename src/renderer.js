5cfdfde0128f614fcf3f9b5377b377e006a47312
Cristian Yanez <cristian.yanez@2brains.cl>
2016-07-03 07:07:18 -0400

    organizing files

--- src/renderer.js
+++ src/renderer.js
@@ -3,7 +3,7 @@
 // All of the Node.js APIs are available in this process.
 const cheerio = require('cheerio')
 const shell = require('shelljs')
-const genius = require('./secret').genius
+const genius = require('../secret').genius
 
 let currentSong = shell.exec('osascript spotify.applescript', {async: true})
 
@@ -11,6 +11,7 @@ currentSong.stdout.on('data', song => {
   let geniusHeaders = new Headers()
   geniusHeaders.append('Authorization', `Bearer ${genius.accessToken}`)
 
+  document.querySelector('.song-name').innerHTML = song
   song = song.replace('Live', '')
 
   fetch(`http://api.genius.com/search?q=${song}`, {
