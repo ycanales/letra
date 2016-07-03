4ca84030f3e8644dee4d2c425e7e106c7397d82f
Cristian Yanez <cristian.yanez@2brains.cl>
2016-07-03 06:27:26 -0400

    removed js code from lyrics

--- renderer.js
+++ renderer.js
@@ -3,11 +3,10 @@
 // All of the Node.js APIs are available in this process.
 const cheerio = require('cheerio')
 const shell = require('shelljs')
-
 const genius = require('./secret').genius
 
-console.log('shelljs')
 let currentSong = shell.exec('osascript spotify.applescript', {async: true})
+
 currentSong.stdout.on('data', song => {
   console.log(song)
   let geniusHeaders = new Headers()
@@ -29,7 +28,7 @@ currentSong.stdout.on('data', song => {
     })
     .then(function(text) {
       let $ = cheerio.load(text)
-      let lyrics = $('.lyrics').text().trim()
+      let lyrics = $('.lyrics').text().trim().replace(/googletag\.cmd\.push\(.*}\);/g, '')
       document.getElementById('lyrics').innerHTML = lyrics
     })
 })
