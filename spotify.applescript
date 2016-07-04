a8ae6e26cae89ee67b97c5e432d08bdff6686ff8
Cristian Yanez <cristian.yanez@2brains.cl>
2016-07-04 02:15:50 -0400

    html changes, documented code

--- spotify.applescript
+++ spotify.applescript
@@ -8,6 +8,6 @@ on getCurrentlyPlayingTrack()
         set currentArtist to artist of current track as string
         set currentTrack to name of current track as string
 
-        return currentArtist & " " & currentTrack
+        return currentArtist & "-" & currentTrack
     end tell
 end getCurrentlyPlayingTrack
--- src/renderer.js
+++ src/renderer.js
@@ -7,17 +7,27 @@ const genius = require('../secret').genius
 
 let currentSong = shell.exec('osascript spotify.applescript', {async: true})
 
-currentSong.stdout.on('data', song => {
+currentSong.stdout.on('data', output => {
+  // ES6 array destructuring assignment
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+  let [artist, song] = output.split('-')
+
+  // Fetch API - Headers
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Headers
   let geniusHeaders = new Headers()
   geniusHeaders.append('Authorization', `Bearer ${genius.accessToken}`)
 
-  document.querySelector('.song-name').innerHTML = song
+  document.querySelector('.song').innerHTML = song
+  document.querySelector('.artist').innerHTML = artist
   song = song.replace('Live', '')
 
-  fetch(`http://api.genius.com/search?q=${song}`, {
+  // Fetch API - Making fetch requests
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Making_fetch_requests
+  fetch(`http://api.genius.com/search?q=${artist}+${song}`, {
     method: 'GET',
     headers: geniusHeaders
   })
+    // json() method of the response returns a Promise
     .then(function(response) {
       return response.json()
     })
