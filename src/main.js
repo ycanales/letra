ecad79c6c3afb7d39a5678ab5b47f090d1eb9092
Cristian Yanez <cristian.yanez@2brains.cl>
2016-07-05 02:13:18 -0400

    menubar tray y estilos

--- src/main.js
+++ src/main.js
@@ -1,16 +1,41 @@
+const path = require('path')
 const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain
+const {app, BrowserWindow, Menu, Tray} = electron
+
+const WIDTH = 570
+const HEIGHT = 650
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let tray = null
 
-function createWindow () {
+function createTray () {
+  tray = new Tray(path.join(__dirname, 'assets', 'menubar@2x.png'))
+  tray.setPressedImage(path.join(__dirname, 'assets', 'menubar-white@2x.png'))
+  createWindow(tray.getBounds())
+
+  tray.on('click', function(event, bounds) {
+    if (mainWindow) {
+      mainWindow.close()
+    } else {
+      createWindow(bounds)
+    }
+  })
+}
+
+
+
+function createWindow (bounds) {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1600, height: 600})
+  mainWindow = new BrowserWindow({
+    width: WIDTH,
+    height: HEIGHT,
+    frame: false,
+    x: bounds.x + bounds.width / 2 - WIDTH / 2,
+    y: bounds.y + bounds.height + 10
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
@@ -30,7 +55,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createTray)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -45,7 +70,7 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    // createWindow()
   }
 })
 
--- src/renderer.js
+++ src/renderer.js
@@ -2,45 +2,52 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const cheerio = require('cheerio')
+const ipc = require('electron').ipcRenderer
 const shell = require('shelljs')
+
 const genius = require('../secret').genius
 
-let currentSong = shell.exec('osascript spotify.applescript', {async: true})
+getLyrics()
 
-currentSong.stdout.on('data', output => {
-  // ES6 array destructuring assignment
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-  let [artist, song] = output.split('-')
+function getLyrics() {
+  let currentSong = shell.exec('osascript spotify.applescript', {async: true})
 
-  // Fetch API - Headers
-  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Headers
-  let geniusHeaders = new Headers()
-  geniusHeaders.append('Authorization', `Bearer ${genius.accessToken}`)
+  currentSong.stdout.on('data', output => {
+    // ES6 array destructuring assignment
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    let [artist, song] = output.split('-')
 
-  document.querySelector('.song').innerHTML = song
-  document.querySelector('.artist').innerHTML = artist
-  song = song.replace('Live', '')
+    // Fetch API - Headers
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Headers
+    let geniusHeaders = new Headers()
+    geniusHeaders.append('Authorization', `Bearer ${genius.accessToken}`)
 
-  // Fetch API - Making fetch requests
-  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Making_fetch_requests
-  fetch(`http://api.genius.com/search?q=${artist}+${song}`, {
-    method: 'GET',
-    headers: geniusHeaders
+    document.querySelector('.song').innerHTML = song
+    document.querySelector('.artist').innerHTML = artist
+    song = song.replace('Live', '')
+    encodedSong = encodeURIComponent(`${artist} ${song}`)
+
+    // Fetch API - Making fetch requests
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Making_fetch_requests
+    fetch(`http://api.genius.com/search?q=${encodedSong}`, {
+      method: 'GET',
+      headers: geniusHeaders
+    })
+      // json() method of the response returns a Promise
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(json) {
+        return fetch(json.response.hits[0].result.url)
+      })
+      .then(function(response) {
+        return response.text()
+      })
+      .then(function(text) {
+        let $ = cheerio.load(text)
+        let lyrics = $('.lyrics').text().trim().replace(/googletag\.cmd\.push\(.*}\);/g, '')
+        document.querySelector('.loading').style.display = 'none'
+        document.getElementById('lyrics').innerHTML = lyrics
+      })
   })
-    // json() method of the response returns a Promise
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(json) {
-      return fetch(json.response.hits[0].result.url)
-    })
-    .then(function(response) {
-      return response.text()
-    })
-    .then(function(text) {
-      let $ = cheerio.load(text)
-      let lyrics = $('.lyrics').text().trim().replace(/googletag\.cmd\.push\(.*}\);/g, '')
-      document.getElementById('lyrics').innerHTML = lyrics
-    })
-})
-
+}
