fa73d3d521880756c879a5a0a94aca4df53f7ca5
Cristian Yáñez <cristian.yanez@2brains.cl>
2017-01-03 22:25:57 -0300

    devtools opens with DEVTOOLS=true env variable

--- src/main.js
+++ src/main.js
@@ -47,7 +47,9 @@ function createWindow (bounds) {
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  if (process.env.DEVTOOLS) {
+    mainWindow.webContents.openDevTools()
+  }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
